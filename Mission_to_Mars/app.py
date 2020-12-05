cb3c898e057cbd92ea66f2bff12f6c504ebdd0d8
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2020-12-04 23:09:56 -0600

    Hemisphere image not working

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import scrape_image
 
@@ -14,10 +14,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", mars=mars_data)
 
 
 # Route that will trigger the scrape function
@@ -26,7 +26,7 @@ def scrape():
 
     # Run the scrape function
     image_data = scrape_image.scrape_info()
-
+    
     # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, image_data, upsert=True)
 
--- Mission_to_Mars/scrape_image.py
+++ Mission_to_Mars/scrape_image.py
@@ -3,61 +3,77 @@ from bs4 import BeautifulSoup as bs
 import time
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import os
+import pandas as pd
 
-
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
+ 
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": ChromeDriverManager().install()}
-    return Browser("chrome", **executable_path, headless=False)
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    return Browser('chrome', **executable_path, headless=False)
 
 
 def scrape_info():
-    browser = init_browser()
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser=Browser('chrome', **executable_path, headless=False)
+    image_data={}
 
-    # Visit nasa.gov
+#grab title and text
+    url ='https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
+    browser.visit(url)
+    soup = bs(browser.html, 'html.parser')
+    news_title= soup.find_all('div', class_='content_title')
+    news_title= news_title[1].a.text
+    news_p = soup.find_all('div', class_='article_teaser_body')
+    news_p = news_p[0].text
+    #Add title and text to image data
+    image_data["news_title"]=news_title
+    image_data["news_p"]=news_p
+
+
+ # Visit nasa.gov
     url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(url)
-    time.sleep(1)
+    time.sleep(2)
      # Scrape page into Soup
     html = browser.html
     soup = bs(html, "html.parser")
-    # BONUS: Find the src for the sloth image
+    # BONUS: Find the src for the mars image
     relative_image_path = soup.find_all('img')[1]["src"]
     featured_image_url = url + relative_image_path
     # Store data in a dictionary
     image_data = {
         "Mars photo": featured_image_url
     }
-    # Close the browser after scraping
-    browser.quit()
-     # Return results
-    return image_data
+   
 
 
 
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url)
-    time.sleep(1)
-    # Scrape page into Soup
+    time.sleep(2)
     html = browser.html
     soup = bs(html, "html.parser")
-    # BONUS: Find the src for the sloth image
+    hemisphere_data=[]
+
     relative_image_path0 = soup.find_all('img')[0]["src"]
     relative_image_path1 = soup.find_all('img')[1]["src"]
     relative_image_path2 = soup.find_all('img')[2]["src"]
     relative_image_path3 = soup.find_all('img')[3]["src"]
-    #featured_image_url = url + relative_image_path
-    # Store data in a dictionary
-    hemisphere_image_data = [
-        {"title": "Valles Marineris Hemisphere", "img_url": "relative_image_path0"},
-        {"title": "Cerberus Hemisphere", "img_url": "relative_image_path1"},
-        {"title": "Schiaparelli Hemisphere", "img_url": "relative_image_path2"},
-        {"title": "Syrtis Major Hemisphere", "img_url": "relative_image_path3"},
+ 
+    hemisphere_data = [
+        {"title": "Valles Marineris Hemisphere", "img_url": {{ relative_image_path0 }} },
+        {"title": "Cerberus Hemisphere", "img_url": {{ relative_image_path1 }} },
+        {"title": "Schiaparelli Hemisphere", "img_url": {{ relative_image_path2 }} },
+        {"title": "Syrtis Major Hemisphere", "img_url": {{ relative_image_path3 }} }
     ]
-    browser.quit()
+    image_data={
+      "hemisphere_image_data": hemisphere_data
+    }
 
     # Return results
-    return hemisphere_image_data
+    return image_data
+
+if __name__=="__main__":
+    print(scrape_info())
+
